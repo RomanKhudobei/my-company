ea49d42d0ae7df44ebb2f4162cf19b46b3024e73
Roman Khudobei <roman.khudobei@gmail.com>
2022-01-11 18:56:19 +0200

    Add db setup

--- src/app/factory.py
+++ src/app/factory.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 
+from .db import db
+
 PKG_NAME = os.path.dirname(os.path.realpath(__file__)).split('/')[-1]
 
 
@@ -9,8 +11,10 @@ def create_app(app_name=PKG_NAME, test_config=None):
     app = Flask(app_name)
 
     if test_config is None:
-        app.config.from_pyfile("config.py", silent=True)
+        app.config.from_pyfile("src/app/config.py")
     else:
         app.config.update(test_config)
+
+    db.init_app(app)
 
     return app
