49334cc0ac8dbf7c127e7e644faf21b525e02a1b
Roman Khudobei <roman.khudobei@gmail.com>
2022-04-08 19:41:45 +0300

    Remove redundant decorators

--- src/auth/jwt_handlers.py
+++ src/auth/jwt_handlers.py
@@ -1,13 +1,10 @@
-from app.jwt import jwt
 from user.models import User
 
 
-@jwt.user_identity_loader
 def user_identity_lookup(user):
     return user.id
 
 
-@jwt.user_lookup_loader
 def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
     return User.query.filter_by(id=identity).one_or_none()
