fdc346ae8364004b9f7f75ac86b998e637b784a8
Roman Khudobei <roman.khudobei@gmail.com>
2022-04-11 12:35:32 +0300

    Create employer relation in models

--- src/company/models.py
+++ src/company/models.py
@@ -10,9 +10,20 @@ class Company(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     address = db.Column(db.String)
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id', use_alter=True), nullable=False)
 
     created_at = db.Column(db.DateTime, nullable=False, default=dt.datetime.now)
+
+    employees = db.relationship(
+        'User',
+        foreign_keys='User.employer_id',
+        backref=db.backref(
+            'employer',
+            uselist=False,
+            lazy=True,
+            cascade="all,delete",
+        )
+    )
 
     def __repr__(self):
         return '<Company ' \
